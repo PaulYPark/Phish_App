7c1276dc2c58b0da9c77f8a0d6098a6e62a3664f
PaulYPark <paulyounghpark13@gmail.com>
2019-10-04 16:56:04 -0700

    Added a comment to MainActivity.java

--- app/src/main/java/edu/uw/tcss450/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/MainActivity.java
@@ -14,6 +14,7 @@ import model.Credentials;
 
 public class MainActivity extends AppCompatActivity {
 
+    // This is Paul
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
